1ab36977daf9caae8446c5a912c8df91a6839561
hanfik27 <hanfik27@gmail.com>
2025-11-28 19:51:20 +0700

    update

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
