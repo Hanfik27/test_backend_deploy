662ba51eedbd20d8ec533132572b071448982a5c
hanfik27 <hanfik27@gmail.com>
2025-11-29 09:35:37 +0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ import itemRoutes from './src/routes/itemRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
